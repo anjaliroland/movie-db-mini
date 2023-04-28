7620982f8e0c34331928c8b4fda4178ced8cc28b
anjaliroland <anjaligroland@gmail.com>
2023-04-28 14:06:07 -0700

    added data

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO movies (movie_name)
+VALUES ("Jurassic Park"),
+       ("Avengers"),
+       ("Avengers: Age of Ultron");
+
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "It's really good!"),
+       (2, "It's good!"),
+       (3, "Ultron's going to kill us all");
